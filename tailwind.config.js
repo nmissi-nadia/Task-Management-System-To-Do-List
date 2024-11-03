ee327593b12eecd7a6293f7b6afdf877c9700726
nmissi-nadia <nmissinadia@gmail.com>
2024-11-03 17:13:04 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,3 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{html,js}",
-    "./public/index.html",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+
 
 
